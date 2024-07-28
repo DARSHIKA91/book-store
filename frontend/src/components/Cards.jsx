bfab44b12bda6d045da42e03ba943528ddf93aa3
DARSHIKA91 <darshikaagrawal9175@gmail.com>
2024-07-28 14:42:26 +0530

    about page

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -6,7 +6,7 @@ function Cards({ item }) {
     <>
     <Link to="/about">
       <div className="mt-4 my-3 p-3">
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+        <div className="card w-92 h-80 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
           <figure>
             <img src={item.image} alt="Shoes" />
           </figure>
